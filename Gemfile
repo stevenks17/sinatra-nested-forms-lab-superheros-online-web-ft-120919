f5250db0e905c0c6e45d5cbf281d6a8ed0d05a07
Steven K <stevenks17@gmail.com>
2020-02-07 14:56:10 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
-# gem "rails"
+ gem "rails"
 
 gem 'sinatra', '1.4.6'
 gem 'rake', '10.4.2'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,20 @@
-#require '../../config/environment'
+require 'sinatra/base'
 
 class App < Sinatra::Base
 
-  set :views, Proc.new { File.join(root, "../views/") }
-  
-  get '/' do
-    erb :super_hero
-  end
+    set :views, Proc.new { File.join(root, "../views/") }
 
-  post '/teams' do
-    @team_name = params[:team][:name]
-    @team_motto = params[:team][:motto]
-    @hero_name = []
-    @hero_power = []
-    @hero_bio = []
-    @team_members = params[:team][:members]
-    @team_members.each do |hero|
-      @hero_name << hero[:name]
-      @hero_power << hero[:power]
-      @hero_bio << hero[:bio]
+    get '/' do
+      erb :'../views/super_hero'
     end
 
-    erb :team
-  end
+    post '/teams' do
+      @team_name = params["team"]["name"]
+      @team_motto = params["team"]["motto"]
+      @team_members = params["team"]["members"]
+
+      erb :'../views/team'
+    end
+
+
 end
